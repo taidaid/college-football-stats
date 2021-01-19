565bb7f20070ecca01a188c2ac386ff7f1f94ceb
Bryan <windsor.bryan@gmail.com>
2021-01-19 12:24:33 +0100

    -  built fetch conferences - added state for conferences - render conferences inside bootstrap container/row

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+import { getConferences } from "./api";
+import "./App.css";
+import ConferenceCard from "./Components/ConferenceCard";
+import { Conference } from "./interfaces";
+
+const App = () => {
+  const [conferences, setConferences] = useState<Conference[]>([]);
+
+  useEffect(() => {
+    getConferences().then((conferencesData) => setConferences(conferencesData));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        College Football Stats
-      </header>
+      <header className="App-header">College Football Stats</header>
+      <Container fluid>
+        <Row>
+          {conferences.map((conf) => (
+            <Col xs="6" md="4" lg="2">
+              <ConferenceCard conference={conf} />
+            </Col>
+          ))}
+          <Col></Col>
+        </Row>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
