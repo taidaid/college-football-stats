00c204ab4b0c1e470ce9d13171228c3c18ca2815
Bryan <windsor.bryan@gmail.com>
2021-01-19 12:23:42 +0100

    created basic Conference Card

---
+++ src/Components/ConferenceCard.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Conference } from "../interfaces";
+
+interface Props {
+  conference: Conference;
+}
+
+const ConferenceCard = ({ conference }: Props) => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Body>
+        <Card.Title>{conference.short_name}</Card.Title>
+        <Button variant="primary">View Teams</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ConferenceCard;
