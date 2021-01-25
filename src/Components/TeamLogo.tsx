a4c1c98279049ff1b4238c3a2bebcef04c75f387
Bryan <windsor.bryan@gmail.com>
2021-01-25 09:17:08 +0100

    - added placeholder/fallback image - preset container size to avoid snapping to size on image load- - added smooth image loading

--- src/Components/TeamLogo.tsx
+++ src/Components/TeamLogo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import ncaaLogo from "../assets/ncaa-4-logo-png-transparent-150-150.png";
+import ncaaLogo from "../assets/ncaa-4-logo-png-transparent.png";
 
 interface Props {
   logos: string[];
@@ -8,6 +8,7 @@ interface Props {
 
 const TeamLogo = ({ logos, dark }: Props) => {
   const [imgSource, setImgSource] = useState<string>("");
+  const [loadedStyles, setLoadedStyles] = useState<{ [key: string]: any }>({});
   const [errored, setErrored] = useState<boolean>(false);
   const setFallbackSource = () => {
     if (!errored) {
@@ -16,29 +17,47 @@ const TeamLogo = ({ logos, dark }: Props) => {
     }
   };
 
+  const handleImagedLoaded = () => {
+    setLoadedStyles({
+      opacity: "1",
+      transitionProperty: "opacity",
+      transitionDuration: ".2s",
+      transitionTimingFunction: "ease-in",
+    });
+  };
+
+  const initialContainerStyles: {
+    position: "relative";
+    [key: string]: string | number;
+  } = {
+    position: "relative",
+    width: "100%",
+    opacity: 0,
+    paddingBottom: "100%",
+  };
+
   useEffect(() => {
     if (logos) {
       setImgSource(dark ? logos[1] : logos[0]);
     }
   }, [dark, logos]);
-  if (logos) {
-    return (
-      <span>
-        <img
-          src={imgSource}
-          onError={setFallbackSource}
-          style={{ width: "100%" }}
-          alt="team logo"
-        ></img>
-      </span>
-    );
-  } else {
-    return (
-      <span>
-        <img src={ncaaLogo} style={{ width: "100%" }} alt="team logo"></img>
-      </span>
-    );
-  }
+
+  return (
+    <div style={Object.assign({}, initialContainerStyles, loadedStyles)}>
+      <img
+        src={imgSource}
+        onError={setFallbackSource}
+        onLoad={handleImagedLoaded}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+        }}
+        alt="team logo"
+      ></img>
+    </div>
+  );
 };
 
 export default TeamLogo;
